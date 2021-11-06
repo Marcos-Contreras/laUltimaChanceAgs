f550f3c4d92244bf31c0d2710ab3c063c648f82b
Marcos-Contreras <marcosalonso2230@gmail.com>
2021-11-06 01:50:10 -0600

    Pantallas de Inicio (Carga al iniciar app), Login y Register

--- app/src/main/java/com/example/laultimachance/Register.java
+++ app/src/main/java/com/example/laultimachance/Register.java
@@ -5,24 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class Register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.Theme_LaUltimaChance);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
 
-    public void btnToLogin(View view) {
+    public void btnHaveAccount(View view) {
         Intent toLogin = new Intent(this, Login.class);
         startActivity(toLogin);
-    }
-
-    public void btnToRegister(View view) {
-        Intent toRegister = new Intent(this, Register.class);
-        startActivity(toRegister);
     }
 }
