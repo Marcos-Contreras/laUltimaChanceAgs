838757cb6b237b2005f15c723ac6219a568bd6df
CaroLugo <carolina18lugo@gmail.com>
2021-11-06 23:47:23 -0600

    Intents

--- app/src/main/java/com/example/laultimachance/Ofertas.java
+++ app/src/main/java/com/example/laultimachance/Ofertas.java
@@ -2,7 +2,9 @@ package com.example.laultimachance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Ofertas extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Ofertas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ofertas);
+    }
+
+    public void btnAñadir(View view) {
+        Intent añadir = new Intent(this, Carro.class);
+        startActivity(añadir);
+    }
+
+    public void descProduct(View view) {
+        Intent des = new Intent(this, ProductoBebidas1.class);
+        startActivity(des);
     }
 }
