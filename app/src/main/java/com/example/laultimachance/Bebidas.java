0aac56d8ad2cbcfde7c7716ae7cd8925d8aea719
COMPAQ <gamac12@hotmail.com>
2021-11-06 23:53:29 -0600

    Mandar a otra activity

--- app/src/main/java/com/example/laultimachance/Bebidas.java
+++ app/src/main/java/com/example/laultimachance/Bebidas.java
@@ -2,7 +2,9 @@ package com.example.laultimachance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Bebidas extends AppCompatActivity {
 
@@ -11,5 +13,13 @@ public class Bebidas extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bebidas);
     }
+    public void btnAñadir (View view) {
+        Intent añadir = new Intent(this, Carro.class);
+        startActivity(añadir);
+    }
 
+    public void descProduct(View view) {
+        Intent des = new Intent(this, ProductoBebidas1.class);
+        startActivity(des);
+    }
 }
